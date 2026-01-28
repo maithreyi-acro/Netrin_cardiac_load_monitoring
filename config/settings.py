ddaddd0722d67e8b5e421f832fb6250c35bdb5df
Ganesh Aadhithya <ganeshaadhithya5@gmail.com>
2026-01-28 14:53:09 +0530

    Update settings configuration

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BIN_MINUTES = BIN_SECONDS / 60.0
 
 # Preprocessing
 INTERP_LIMIT_SEC = 10
-SMOOTH_WINDOW_SEC = 5
+SMOOTH_WINDOW_SEC = 9
 
 # Training load
 BANISTER_COEFF = 1.90
@@ -19,7 +19,4 @@ TRIMP_SCALE = 0.64
 
 # Zones
 ZONE_BOUNDS = [(0.0,0.5),(0.5,0.6),(0.6,0.7),(0.7,0.8),(0.8,0.9),(0.9,1.0)]
-
-# Default athlete parameters (used if not provided in data)
-DEFAULT_WEIGHT = 70.0  
-DEFAULT_AGE = 25       
+     
